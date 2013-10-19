45f4ed8636d038ff5dede69b1693ffd631e6c504
Edwin Derks <ederks85@gmail.com>
2013-10-19 08:43:32 +0200

    deleted dummy handler since it is no longer required

--- src/main/java/nl/iqfit/logic/payment/DummyPaymentHandlerSB.java
+++
@@ -1,8 +0,0 @@
-package nl.iqfit.logic.payment;
-
-import javax.enterprise.inject.Alternative;
-
-@Alternative
-public class DummyPaymentHandlerSB implements PaymentHandler {
-
-}
--- src/main/java/nl/iqfit/logic/payment/MolliePaymentHandlerSB.java
+++ src/main/java/nl/iqfit/logic/payment/MolliePaymentHandlerSB.java
@@ -1,8 +1,5 @@
 package nl.iqfit.logic.payment;
 
-import javax.enterprise.inject.Alternative;
-
-@Alternative
 public class MolliePaymentHandlerSB implements PaymentHandler {
 
 }
